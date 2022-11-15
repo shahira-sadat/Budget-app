6031c22ed7d9ca0cf6b12c524d68f7ffc6b2fdd0
shahira-sadat <shahira.sadat1@gmail.com>
2022-11-15 04:51:31 +0430

    Fix linters

--- spec/views/spendings/edit.html.erb_spec.rb
+++ spec/views/spendings/edit.html.erb_spec.rb
@@ -1,18 +1,18 @@
 require 'rails_helper'
 
-RSpec.describe "spendings/edit", type: :view do
-  let(:spending) {
-    Spending.create!()
-  }
+RSpec.describe 'spendings/edit', type: :view do
+  let(:spending) do
+    Spending.create!
+  end
 
   before(:each) do
     assign(:spending, spending)
   end
 
-  it "renders the edit spending form" do
+  it 'renders the edit spending form' do
     render
 
-    assert_select "form[action=?][method=?]", spending_path(spending), "post" do
+    assert_select 'form[action=?][method=?]', spending_path(spending), 'post' do
     end
   end
 end
--- spec/views/spendings/new.html.erb_spec.rb
+++ spec/views/spendings/new.html.erb_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
-RSpec.describe "spendings/new", type: :view do
+RSpec.describe 'spendings/new', type: :view do
   before(:each) do
-    assign(:spending, Spending.new())
+    assign(:spending, Spending.new)
   end
 
-  it "renders new spending form" do
+  it 'renders new spending form' do
     render
 
-    assert_select "form[action=?][method=?]", spendings_path, "post" do
+    assert_select 'form[action=?][method=?]', spendings_path, 'post' do
     end
   end
 end
