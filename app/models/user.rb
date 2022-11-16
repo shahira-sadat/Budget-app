87cee68d2208be1af400345b961e84113960bbc4
shahira-sadat <shahira.sadat1@gmail.com>
2022-11-17 02:23:43 +0430

    Add authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  
+  validates :name, presence: true, length: { maximum: 100 }
+
+  has_many :groups, dependent: :delete_all
+  has_many :spendings, dependent: :delete_all
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  resources :spendings
-  resources :groups
-  resources :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  devise_for :users
+  root 'groups#index'
+  resources :groups, only: [:index, :show, :new, :create, :destroy] do
+    resources :spendings, only: [:index, :new, :create, :destroy]
+  end
 end
