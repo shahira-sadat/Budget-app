4caa3812636249ba13f5132724feea39404504f7
shahira-sadat <shahira.sadat1@gmail.com>
2022-11-17 17:08:19 +0430

    Add tests

---
+++ spec/spending_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe Spending, type: :model do
+  before :each do
+    @user = User.new(name: 'Ahmad', email: 'ahmad@gmail.com', password: 123456, password_confirmation: 123456)
+    @user.save
+    @group = Group.new(name: 'Group', icon: 'icon link', user_id: @user.id)
+    @group.save
+    @spending = Spending.new(name: 'Spending', amount: '20', user_id: @user.id, group_id: @group.id)
+  end
+
+  it 'is valid' do
+    expect(@spending).to be_valid
+  end
+
+  it 'name should be present' do
+    @spending.name = nil
+    expect(@spending).to_not be_valid
+  end
+
+  it 'amount should be present' do
+    @spending.amount = nil
+    expect(@spending).to_not be_valid
+  end
+end
