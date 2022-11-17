11b63487c4b688243b3fedb4df07355ed93460b9
shahira-sadat <shahira.sadat1@gmail.com>
2022-11-17 06:26:09 +0430

    Solve issue with authentication

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'groups#index'
   resources :groups, only: [:index, :show, :new, :create, :destroy] do
-    resources :spendings, only: [:index, :new, :create, :destroy]
+    resources :spendings, only: [:index]
   end
+  resources :spendings, only: [:new, :create, :destroy]
 end
