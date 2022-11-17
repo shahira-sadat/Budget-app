1ee9a4f08d8ece84efc45d3676fda038aca44266
shahira-sadat <shahira.sadat1@gmail.com>
2022-11-17 06:21:47 +0430

    Create required actions in group controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,25 @@
 class GroupsController < ApplicationController
   def index
     @user = current_user
+    @groups = @user.groups
+  end
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new(group_params)
+    @group.user_id = current_user.id
+
+    render :new unless @group.save
+
+    redirect_to groups_path
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name, :icon)
   end
 end
