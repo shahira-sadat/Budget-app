02b92052bf02908ad4f5c857c1ee8f5798275cda
shahira-sadat <shahira.sadat1@gmail.com>
2022-11-17 17:09:10 +0430

    Add model tests

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Group, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before :each do
+    @user = User.new(name: 'Ahmad', email: 'ahmad@gmail.com', password: 123456, password_confirmation: 123456)
+    @user.save
+    @group = Group.new(name: 'Group', icon: 'icon link', user_id: @user.id)
+  end
+
+  it 'is valid' do
+    expect(@group).to be_valid
+  end
+
+  it 'name should be present' do
+    @group.name = nil
+    expect(@group).to_not be_valid
+  end
+
+  it 'icon should be present' do
+    @group.icon = nil
+    expect(@group).to_not be_valid
+  end
 end
